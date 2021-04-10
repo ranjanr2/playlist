aa39fcf0ba9688669c39aa6bf24b16c341f657a2
Rohit Ranjan <rohit.ranjan@cognizant.com>
2021-04-10 10:58:06 -0400

    Adding the initial project

---
+++ src/test/java/playlist/Playlist/PlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package playlist.Playlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
