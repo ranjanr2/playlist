7794634faddd6d6766d2f9ce12783c917a6c5711
Rohit Ranjan <rohit.ranjan@cognizant.com>
2021-04-10 14:23:58 -0400

    Working Applicaiton instance and Postgres Intance and Tests running

--- src/test/java/playlist/Playlist/PlaylistTestIT.java
+++ src/test/java/playlist/Playlist/PlaylistTestIT.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class PlaylistTestIT {
     @Autowired
     MockMvc mockMvc;
