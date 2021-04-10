fd1490d4a82bab42bd6b54fad372162dcb8d24bc
Rohit Ranjan <rohit.ranjan@cognizant.com>
2021-04-10 11:10:37 -0400

    RED TEST | create a playlist

---
+++ src/test/java/playlist/Playlist/PlaylistTestIT.java
@@ -0,0 +1,40 @@
+package playlist.Playlist;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+public class PlaylistTestIT {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext
+    public void createAPlaylist() throws Exception {
+        PlaylistDTO playlistDto = new PlaylistDTO("MyPlaylist");
+        mockMvc.perform(post("/Playlist")
+                .content(objectMapper.writeValueAsString(playlistDto))
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isCreated());
+
+    }
+}
